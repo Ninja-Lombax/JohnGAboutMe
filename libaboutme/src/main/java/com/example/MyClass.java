63b655149980ce22129666e8ddf78debcc87e9e2
Ninja-Lombax <godzillakotm80@gmail.com>
2016-09-06 13:18:43 -0500

    Initial commit

---
+++ libaboutme/src/main/java/com/example/MyClass.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class MyClass {
+    public static void main(String[] args)
+    {
+        String myName = "John Grasser";
+        String major = "Information Technology";
+        double gpa = 3.75;
+        char grade = 'A';
+        int started = 2005;
+        System.out.println("Hello, my name is " + myName);
+        System.out.println("I am majoring in " + major);
+        System.out.println("My goal this semester is to get a " + gpa +
+                " or higher for my GPA");
+        System.out.println("The grade I hope to get in this class is an " + grade);
+        System.out.println("I first started at MCC in " + started);
+    }
+}
